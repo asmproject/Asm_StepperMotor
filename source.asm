6deb71133a6d038166d107826df2d25beadd4467
mariammaher11 <47921154+mariammaher11@users.noreply.github.com>
2021-01-05 17:35:55 +0200

    Update source.asm

--- source.asm
+++ source.asm
@@ -18,6 +18,9 @@ ORG 100H         ;starts code at address 100H
   
  
 START:          ;THE MAIN CODE
+IN AL, PORTB   ;Copies value of port B to AL (the value of the 8 bits of portB)
+ CMP AL,01H     ; compares Al with 000000001B (which is the value results from logicstate)
+ JE FULL 
 HALF:
 HALFCW:    ;Half mode clockwise 8steps each step is 45ú
    MOV AL, 00001000B  ;moves 08H to AL
